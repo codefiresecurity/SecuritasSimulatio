247c4f20d21b2fb03ea43b65cfcb83d099889b1b
Ryan Wessels <ryan.wessels@gmail.com>
2025-04-14 13:17:23 -0500

    added re

--- webInterfaceOnly/app.py
+++ webInterfaceOnly/app.py
@@ -14,6 +14,7 @@ import json
 import datetime
 import mysql.connector
 import bcrypt
+import re
 
 # Set up logging
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
